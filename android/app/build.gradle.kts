cc1c6f9eb25f34058697d89237593c50b4d92578
Andrew Tridgell <andrew@tridgell.net>
2026-01-12 11:45:41 +1100

    v1.10.5

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         applicationId = "nz.co.tracker.windsurfer"
         minSdk = 26
         targetSdk = 35
-        versionCode = 65
-        versionName = "1.10.4"
+        versionCode = 67
+        versionName = "1.10.5"
 
         // Add version info as build config fields
         buildConfigField("String", "GIT_HASH", "\"${getGitHash()}\"")
--- wear/app/build.gradle.kts
+++ wear/app/build.gradle.kts
@@ -35,8 +35,8 @@ android {
         applicationId = "nz.co.tracker.windsurfer"
         minSdk = 30  // Wear OS 3.0 minimum
         targetSdk = 35
-        versionCode = 66
-        versionName = "1.10.4"
+        versionCode = 68
+        versionName = "1.10.5"
 
         // Add version info as build config fields
         buildConfigField("String", "GIT_HASH", "\"${getGitHash()}\"")
