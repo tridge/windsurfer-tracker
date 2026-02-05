eac87fdd0996050ec05454a8466eed25df8a58f7
Andrew Tridgell <andrew@tridgell.net>
2026-02-05 20:03:39 +1100

    v1.10.16

--- wear/app/build.gradle.kts
+++ wear/app/build.gradle.kts
@@ -35,8 +35,8 @@ android {
         applicationId = "nz.co.tracker.windsurfer"
         minSdk = 30  // Wear OS 3.0 minimum
         targetSdk = 35
-        versionCode = 84
-        versionName = "1.10.14"
+        versionCode = 88
+        versionName = "1.10.16"
 
         // Add version info as build config fields
         buildConfigField("String", "GIT_HASH", "\"${getGitHash()}\"")
