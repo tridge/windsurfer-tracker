3586db7cec46453f63e1750afdb6b2e97181717a
Andrew Tridgell <andrew@tridgell.net>
2026-02-17 05:26:04 +1100

    v1.10.28

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         applicationId = "nz.co.tracker.windsurfer"
         minSdk = 26
         targetSdk = 35
-        versionCode = 109
-        versionName = "1.10.27"
+        versionCode = 111
+        versionName = "1.10.28"
 
         // Add version info as build config fields
         buildConfigField("String", "GIT_HASH", "\"${getGitHash()}\"")
